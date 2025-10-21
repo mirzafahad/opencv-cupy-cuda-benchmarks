ffaf63b786a802ca52bf894b647a36ce5678611f
Fahad Mirza <fahad.mirza2@7-11.com>
2025-10-21 03:44:10 -0500

    Added stream.waitForCompletion() before measuring gpu timing.

--- src/benchmark/bg_subtraction_benchmark.py
+++ src/benchmark/bg_subtraction_benchmark.py
@@ -27,7 +27,7 @@ def run_profiling(image_file: str, video_file: str, static_image_iteration: int)
     # Initialize the cpu based background subtractor.
     bg_subtractor = cv2.bgsegm.createBackgroundSubtractorMOG()
 
-    # Now run the subtractor using the same image for 100 iterations.
+    # Now run the subtractor using the same image for multi iterations.
     start_time_cpu_image = time.perf_counter()
     for _ in range(static_image_iteration):
         bg_subtractor.apply(image, learningRate=0.1)
@@ -59,8 +59,8 @@ def run_profiling(image_file: str, video_file: str, static_image_iteration: int)
     # First check if OpenCV-CUDA is available.
     if hasattr(cv2, "cuda") and cv2.cuda.getCudaEnabledDeviceCount() > 0:
         bg_subtractor = cv2.cuda.createBackgroundSubtractorMOG()
-        stream: Stream = cv2.cuda_Stream()
-        gpu_frame: GpuMat = cv2.cuda_GpuMat()
+        stream: Stream = cv2.cuda.Stream()
+        gpu_frame: GpuMat = cv2.cuda.GpuMat()
         logger.debug(f"Using GPU OpenCV with {cv2.cuda.getCudaEnabledDeviceCount()} CUDA device(s)")
     else:
         logger.error("OpenCV-CUDA not available!")
@@ -71,11 +71,13 @@ def run_profiling(image_file: str, video_file: str, static_image_iteration: int)
     # Now run the subtractor for 100 times.
     for _ in range(static_image_iteration):
         # Upload to GPU.
-        gpu_frame.upload(image)
+        gpu_frame.upload(image, stream=stream)
         # Get a mask from the subtraction.
         gpu_foreground_mask = bg_subtractor.apply(gpu_frame, learningRate=0.1, stream=stream)
         # Download the result from the GPU to CPU.
-        gpu_foreground_mask.download()
+        cpu_fg_mask = gpu_foreground_mask.download(stream=stream)
+    # Wait for all GPU operations to complete before stopping the timer.
+    stream.waitForCompletion()
     time_gpu_image = time.perf_counter() - start_time_gpu_image
 
     # Now run the subtractor for the video.
@@ -91,11 +93,13 @@ def run_profiling(image_file: str, video_file: str, static_image_iteration: int)
         if not ret:
             logger.debug("End of video.")
             break
-        gpu_frame.upload(frame)
-        # Get a mask from the subtraction
+        gpu_frame.upload(frame, stream=stream)
+        # Get a mask from the subtraction.
         gpu_foreground_mask = bg_subtractor.apply(gpu_frame, learningRate=0.1, stream=stream)
         # Download the result from the GPU to CPU.
-        gpu_foreground_mask.download()
+        cpu_fg_mask = gpu_foreground_mask.download(stream=stream)
+    # Wait for all GPU operations to complete before stopping the timer.
+    stream.waitForCompletion()
     time_gpu_video = time.perf_counter() - start_time_gpu_video
 
     logger.debug(f"CPU: Static image for {static_image_iteration} iterations - {time_cpu_image}")
